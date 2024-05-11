008d69ad8bec5028555a437d6a3bd23f3ad81ee0
Gary Ma <gary.ma@entelect.co.za>
2024-05-11 23:35:53 +0200

    first commit with synchronous

---
+++ src/main/java/com/gary/springboot/async/PurchaseOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gary.springboot.async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchaseOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gary/springboot/async/PurchaseOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gary.springboot.async;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
