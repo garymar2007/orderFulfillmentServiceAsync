49ea91d9b7b8f9c31902f3d5ef96d5d6f4ea4cef
Gary Ma <gary.ma@entelect.co.za>
2024-05-14 23:04:40 +0200

    Configure Async.

--- src/main/java/com/gary/springboot/async/service/OrderFulfillmentService.java
+++ src/main/java/com/gary/springboot/async/service/OrderFulfillmentService.java
@@ -41,28 +41,31 @@ public class OrderFulfillmentService {
         return order;
     }
 
-    //@Async("asyncTaskExecutor")
+    @Async("asyncTaskExecutor")
     public void notifyUser(Order order) throws InterruptedException {
         Thread.sleep(4000L);
         log.info("Notified to the user " + Thread.currentThread().getName());
     }
-    //@Async("asyncTaskExecutor")
+
+    @Async("asyncTaskExecutor")
     public void assignVendor(Order order) throws InterruptedException {
         Thread.sleep(5000L);
         log.info("Assign order to vendor " + Thread.currentThread().getName());
     }
-    //@Async("asyncTaskExecutor")
+
+    @Async("asyncTaskExecutor")
     public void packaging(Order order) throws InterruptedException {
         Thread.sleep(2000L);
         log.info("Order packaging completed " + Thread.currentThread().getName());
     }
-   // @Async("asyncTaskExecutor")
+
+    @Async("asyncTaskExecutor")
     public void assignDeliveryPartner(Order order) throws InterruptedException {
         Thread.sleep(10000L);
         log.info("Delivery partner assigned " + Thread.currentThread().getName());
     }
 
-    //@Async("asyncTaskExecutor")
+    @Async("asyncTaskExecutor")
     public void assignTrailerAndDispatch(Order order) throws InterruptedException {
         Thread.sleep(3000L);
         log.info("Trailer assigned and Order dispatched " + Thread.currentThread().getName());
